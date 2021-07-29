4e97ccc37f0b93e8b8568706a2f1261b846c9d8c
Mr_Starkk <yashshr09@gmail.com>
2021-07-29 23:36:07 +0530

    finalised functionality

--- library.js
+++ library.js
@@ -4,8 +4,15 @@ function book(title,author,noOfPages,readStatus){
     this.noOfPages = noOfPages,
     this.readStatus = readStatus
 }
-book.prototype.info = function(){
-    return `${this.title} by ${this.author}, ${this.noOfPages} pages, ${this.readStatus}`
+    book.prototype.info = function(){
+        return `${this.title} by ${this.author}, ${this.noOfPages} pages, ${this.readStatus}`
+}
+    book.prototype.readStatusChange = function(){
+        if(this.readStatus == 'true'){
+            this.readStatus = 'false';
+        } else if(this.readStatus == 'false'){
+            this.readStatus = 'true';
+        }
 }
 
 let myLibrary = [];
@@ -19,7 +26,6 @@ function addBookToLibrary(){
         const authorInput = document.getElementById('author-input');
         const pagesInput = document.getElementById('pages-input');
         const readStatus = document.getElementById('read-status');
-            console.log(titleInput.value, authorInput.value, pagesInput.value, readStatus.checked)
             let aBook = new book(`${titleInput.value}`,`${authorInput.value}`,`${pagesInput.value}`,`${readStatus.checked}`);
             myLibrary.push(aBook);
             clearDisplay();
@@ -53,14 +59,75 @@ function displayBook(){
     for(i=0; i<myLibrary.length;i++){
         const bookDisplay = document.getElementById('book-display');
         const div = document.createElement('div');
-        div.id = 'template';
-        div.innerHTML = `${myLibrary[i].title} <br> By ${myLibrary[i].author} <br> ${myLibrary[i].noOfPages} pages <br> <button id='read'>Read</button>`;
+        div.id = 'bookDiv';
+        let titleDiv = document.createElement('div');
+        let authorDiv = document.createElement('div');
+        let pagesDiv = document.createElement('div');
+        let readButton = document.createElement('button');
+        let bookDivClose = document.createElement('span');
+
+            bookDivClose.classList.add('bookDiv-close');
+            bookDivClose.textContent = 'X';
+            readButton.id = 'read'; 
+            titleDiv.textContent = `${myLibrary[i].title}`;
+            authorDiv.textContent = `${myLibrary[i].author}`;
+            pagesDiv.textContent = `${myLibrary[i].noOfPages} pages`;
+            function readButtonStatus(){
+                if(myLibrary[i].readStatus == 'true'){
+                    return 'Read'
+                }else {return 'Not Read Yet'};
+            }
+            readButton.textContent =  readButtonStatus();
+            
+            div.appendChild(titleDiv);
+            div.appendChild(authorDiv);
+            div.appendChild(pagesDiv);
+            div.appendChild(readButton);
+            div.appendChild(bookDivClose);
+
         bookDisplay.appendChild(div);
+
+        readButton.addEventListener('click', (e) => {
+            let bookName = e.target.parentNode.firstChild.textContent;
+             for(i=0; i<myLibrary.length; i++){
+                if(bookName == myLibrary[i].title){
+                    myLibrary[i].readStatusChange();
+                }
+             }
+            if(readButton.textContent == 'Read'){
+                readButton.textContent = 'Not Read Yet';
+            } else if(readButton.textContent == 'Not Read Yet'){
+                readButton.textContent = 'Read';
+            }
+        })
+
+        bookDivClose.addEventListener('click', (e) => {
+            let bookName = e.target.parentNode.firstChild.textContent;
+             for(i=0; i<myLibrary.length; i++){
+                if(bookName == myLibrary[i].title){
+                    myLibrary.splice(i,1);
+                }
+             }
+             clearDisplay();
+             displayBook();
+        })
+
+        // div.addEventListener('mouseover', (e) => {
+        //     const bookInfo = document.getElementById('book-info');
+        //     // let bookInfoTitle = `${myLibrary[i].title}`;
+        //     // let bookInfoAuthor = `${myLibrary[i].author}`;
+        //     // let bookInfoPages = `${myLibrary[i].noOfPages} pages`;
+        //         console.log(e.target.parentNode)
+        //         // bookInfo.appendChild(bookInfoTitle);
+        //         // bookInfo.appendChild(bookInfoAuthor);
+        //         // bookInfo.appendChild(bookInfoPages);
+        // })
     }
 }
 
 
-    let theHobbit = new book('The Hobbit', 'J.R.R Tolkein', '306', 'not read yet');
+    let theHobbit = new book('The Hobbit', 'J.R.R Tolkein', '306', 'true');
     myLibrary.push(theHobbit);
-        myLibrary.forEach(element => console.table(element));
     displayBook();
+
+    
